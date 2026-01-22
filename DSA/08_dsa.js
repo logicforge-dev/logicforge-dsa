8ac6a19883729d648e0ee2c4fec980763044fef3
Ajeet Kumar <48330007+logicforge-dev@users.noreply.github.com>
2026-01-22 23:08:39 +0530

    Create 08_dsa.js

---
+++ DSA/08_dsa.js
@@ -0,0 +1,51 @@
+// # Date: 22/01/2026
+
+// Search in Rotated Sorted Array
+// Medium
+// Company Tags
+// Hints
+// You are given an array of length n which was originally sorted in ascending order. It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+// [3,4,5,6,1,2] if it was rotated 4 times.
+// [1,2,3,4,5,6] if it was rotated 6 times.
+// Given the rotated sorted array nums and an integer target, return the index of target within nums, or -1 if it is not present.
+
+// You may assume all elements in the sorted rotated array nums are unique,
+
+// A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+// Example 1:
+
+// Input: nums = [3,4,5,6,1,2], target = 1
+
+// Output: 4
+
+
+var search = function(nums, target) {
+    let left = 0, right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] === target) return mid;
+
+        // Left half is sorted
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        // Right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
+
