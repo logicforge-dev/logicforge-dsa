bc08af29642cfb4f734049905fad2ae6d327c0ba
Ajeet Kumar <48330007+logicforge-dev@users.noreply.github.com>
2026-01-23 22:58:32 +0530

    Create 09_dsa.js

---
+++ DSA/09_dsa.js
@@ -0,0 +1,50 @@
+// #Date:23/01/2026
+
+// 3Sum
+// Medium
+// Company Tags
+// Hints
+// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0, and the indices i, j and k are all distinct.
+
+// The output should not contain any duplicate triplets. You may return the output and the triplets in any order.
+
+// Example 1:
+
+// Input: nums = [-1,0,1,2,-1,-4]
+
+// Output: [[-1,-1,2],[-1,0,1]]
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        // Skip duplicate fixed elements
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                // Skip duplicates for left & right
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+
+                left++;
+                right--;
+            } 
+            else if (sum < 0) {
+                left++;
+            } 
+            else {
+                right--;
+            }
+        }
+    }
+    return result;
+};
